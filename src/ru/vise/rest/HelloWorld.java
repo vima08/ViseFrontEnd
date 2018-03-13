15287e835ffdf3a0928fb0fb798ddc977e75128b
Averyanov Artem <Averyanovartem@mail.ru>
2018-03-13 23:35:38 +0300

    backend simple ego plotting

--- src/ru/vise/rest/HelloWorld.java
+++ src/ru/vise/rest/HelloWorld.java
@@ -4,6 +4,10 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
+import altr.BackEnd;
+import com.google.gson.Gson;
+
+import static sun.plugin2.util.PojoUtil.toJson;
 
 // The Java class will be hosted at the URI path "/helloworld"
 @Path("/helloworld")
@@ -14,6 +18,11 @@ public class HelloWorld {
     @Produces("text/plain")
     public String getClichedMessage(@QueryParam("test") Integer test, @QueryParam("q") String q) {
         // Return some cliched textual content
-        return "Hi";//TestGetter.get().toString() + test.toString() + " " + q;
+        return new Gson().toJson(BackEnd.runSimpleEgo(10, "ParetoDistribution", 0.1, 1,
+                20, 100, 10, 101,
+                0.5, -250, 100, 10));
+//        return "Hi";//TestGetter.get().toString() + test.toString() + " " + q;
     }
+
+
 }
