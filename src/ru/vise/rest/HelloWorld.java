f9cd08b72ce558bfde2c210c0c7933b577a30c1b
Averyanov Artem <Averyanovartem@mail.ru>
2018-03-15 20:14:17 +0300

    parameters for SimpleEgo

--- src/ru/vise/rest/HelloWorld.java
+++ src/ru/vise/rest/HelloWorld.java
@@ -16,11 +16,28 @@ public class HelloWorld {
     @GET
     // The Java method will produce content identified by the MIME Media type "text/plain"
     @Produces("text/plain")
-    public String getClichedMessage(@QueryParam("test") Integer test, @QueryParam("q") String q) {
+    public String getClichedMessage(@QueryParam("test") Integer test, @QueryParam("q") String q,
+                                    @QueryParam("capital") Double capital,
+                                    @QueryParam("mu") Double mu,
+                                    @QueryParam("sigma") Double sigma,
+                                    @QueryParam("k") Double k,
+                                    @QueryParam("stepNumber") Integer stepNumber,
+                                    @QueryParam("iteration") Integer iteration,
+                                    @QueryParam("peopleCount") Integer peopleCount,
+                                    @QueryParam("majorityThreshold") Double majorityThreshold,
+                                    @QueryParam("var") Integer var,
+                                    @QueryParam("distrib") String distrib,
+                                    @QueryParam("start") Integer start,
+                                    @QueryParam("finish") Integer finish,
+                                    @QueryParam("step")Integer step,
+                                    @QueryParam("multiple") Double multiple) {
         // Return some cliched textual content
-        return new Gson().toJson(BackEnd.runSimpleEgo(10, "ParetoDistribution", 0.1, 1,
-                20, 100, 10, 101,
-                0.5, -250, 100, 10));
+        return new Gson().toJson(BackEnd.runSimpleEgo(capital, "ParetoDistribution", mu, sigma,
+                k, iteration, stepNumber, peopleCount,
+                0.5, start, finish, step));
+//        return new Gson().toJson(BackEnd.runSimpleEgo(10, "ParetoDistribution", 0.1, 1,
+//                20, 100, 10, 101,
+//                0.5, -250, 100, 10));
 //        return "Hi";//TestGetter.get().toString() + test.toString() + " " + q;
     }
 
